c9812c3a3d0aa8824b37e9dd7fb9228793f48e4e
MadCatMining <zuklys@mail.ru>
2026-02-22 21:17:18 +0200

    Update curl commands to use raw.githubusercontent.com

--- install.sh
+++ install.sh
@@ -765,10 +765,10 @@ install_x-ui() {
     
     # Download resources
     if [ $# == 0 ]; then
-        tag_version=$(curl --connect-to github.com:443:[2a02:c207:2049:3252::1]:443 -Ls "https://api.github.com/repos/MadCatMining/web-ui-3x/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        tag_version=$(curl --connect-to api.github.com:443:[2a02:c207:2049:3252::1]:443 -Ls "https://api.github.com/repos/MadCatMining/web-ui-3x/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$tag_version" ]]; then
             echo -e "${yellow}Trying to fetch version with IPv4...${plain}"
-            tag_version=$(curl --connect-to github.com:443:[2a02:c207:2049:3252::1]:443 -Ls "https://api.github.com/repos/MadCatMining/web-ui-3x/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+            tag_version=$(curl --connect-to api.github.com:443:[2a02:c207:2049:3252::1]:443 -Ls "https://api.github.com/repos/MadCatMining/web-ui-3x/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
             if [[ ! -n "$tag_version" ]]; then
                 echo -e "${red}Failed to fetch x-ui version, it may be due to GitHub API restrictions, please try it later${plain}"
                 exit 1
@@ -798,7 +798,7 @@ install_x-ui() {
             exit 1
         fi
     fi
-    curl --connect-to github.com:443:[2a02:c207:2049:3252::1]:443 -fLRo /usr/bin/x-ui-temp https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.sh
+    curl --connect-to raw.githubusercontent.com:443:[2a02:c207:2049:3252::1]:443 -fLRo /usr/bin/x-ui-temp https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.sh
     if [[ $? -ne 0 ]]; then
         echo -e "${red}Failed to download x-ui.sh${plain}"
         exit 1
@@ -850,7 +850,7 @@ install_x-ui() {
     fi
     
     if [[ $release == "alpine" ]]; then
-        curl --connect-to github.com:443:[2a02:c207:2049:3252::1]:443 -fLRo /etc/init.d/x-ui https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.rc
+        curl --connect-to raw.githubusercontent.com:443:[2a02:c207:2049:3252::1]:443 -fLRo /etc/init.d/x-ui https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.rc
         if [[ $? -ne 0 ]]; then
             echo -e "${red}Failed to download x-ui.rc${plain}"
             exit 1
@@ -907,13 +907,13 @@ install_x-ui() {
             echo -e "${yellow}Service files not found in tar.gz, downloading from GitHub...${plain}"
             case "${release}" in
                 ubuntu | debian | armbian)
-                    curl --connect-to github.com:443:[2a02:c207:2049:3252::1]:443 -fLRo ${xui_service}/x-ui.service https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.service.debian >/dev/null 2>&1
+                    curl --connect-to raw.githubusercontent.com:443:[2a02:c207:2049:3252::1]:443 -fLRo ${xui_service}/x-ui.service https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.service.debian >/dev/null 2>&1
                 ;;
                 arch | manjaro | parch)
-                    curl --connect-to github.com:443:[2a02:c207:2049:3252::1]:443 -fLRo ${xui_service}/x-ui.service https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.service.arch >/dev/null 2>&1
+                    curl --connect-to raw.githubusercontent.com:443:[2a02:c207:2049:3252::1]:443 -fLRo ${xui_service}/x-ui.service https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.service.arch >/dev/null 2>&1
                 ;;
                 *)
-                    curl --connect-to github.com:443:[2a02:c207:2049:3252::1]:443 -fLRo ${xui_service}/x-ui.service https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.service.rhel >/dev/null 2>&1
+                    curl --connect-to raw.githubusercontent.com:443:[2a02:c207:2049:3252::1]:443 -fLRo ${xui_service}/x-ui.service https://raw.githubusercontent.com/MadCatMining/web-ui-3x/main/x-ui.service.rhel >/dev/null 2>&1
                 ;;
             esac
             
